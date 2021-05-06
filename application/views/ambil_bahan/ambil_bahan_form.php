d42f860ab2c27edc336b377d39cb867b81903f8f
Anhar Ahyahuda <a.ahyahuda@gmail.com>
2021-05-06 14:14:24 +0700

    add datepicker

--- application/views/ambil_bahan/ambil_bahan_form.php
+++ application/views/ambil_bahan/ambil_bahan_form.php
@@ -6,6 +6,16 @@
     <?php $this->load->view('template/backend/header') ?>
     <?php $this->load->view('template/backend/navbar') ?>
     <?php $this->load->view('template/backend/sidebar') ?>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $(function() {
+            $("#tgl_keluar").datepicker({
+                dateFormat: 'yy-mm-dd'
+            });
+        });
+    </script>
 
 <body>
     <!-- Content Wrapper. Contains page content -->
--- application/views/qc/qc_form.php
+++ application/views/qc/qc_form.php
@@ -1,58 +1,60 @@
 <!doctype html>
 <html>
-    <head>
+
+<head>
     <title>Form</title>
     <?php $this->load->view('template/backend/header') ?>
     <?php $this->load->view('template/backend/navbar') ?>
     <?php $this->load->view('template/backend/sidebar') ?>
-<body>
-  <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-<!-- Content Header (Page header) -->
-    <div class="content-header">
-    <div class="container-fluid">
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
 
-<!-- Main content -->
-<section class="content">
-        <h2 style="margin-top:0px">Qc <?php echo $button ?></h2>
-        <form action="<?php echo $action; ?>" method="post">
-	    <div class="form-group">
-            <label for="int">Id Detail Spk <?php echo form_error('id_detail_spk') ?></label>
-            <input type="text" class="form-control" name="id_detail_spk" id="id_detail_spk" placeholder="Id Detail Spk" value="<?php echo $id_detail_spk; ?>" />
+<body>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+            </div><!-- /.container-fluid -->
         </div>
-	    <div class="form-group">
-            <label for="varchar">No Spk <?php echo form_error('no_spk') ?></label>
-            <input type="text" class="form-control" name="no_spk" id="no_spk" placeholder="No Spk" value="<?php echo $no_spk; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">No Item <?php echo form_error('no_item') ?></label>
-            <input type="text" class="form-control" name="no_item" id="no_item" placeholder="No Item" value="<?php echo $no_item; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="date">Tgl Masuk <?php echo form_error('tgl_masuk') ?></label>
-            <input type="text" class="form-control" name="tgl_masuk" id="tgl_masuk" placeholder="Tgl Masuk" value="<?php echo $tgl_masuk; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="int">Qty <?php echo form_error('qty') ?></label>
-            <input type="text" class="form-control" name="qty" id="qty" placeholder="Qty" value="<?php echo $qty; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Status <?php echo form_error('status') ?></label>
-            <input type="text" class="form-control" name="status" id="status" placeholder="Status" value="<?php echo $status; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Ket <?php echo form_error('ket') ?></label>
-            <input type="text" class="form-control" name="ket" id="ket" placeholder="Ket" value="<?php echo $ket; ?>" />
-        </div>
-	    <input type="hidden" name="id" value="<?php echo $id; ?>" /> 
-	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
-	    <a href="<?php echo site_url('qc') ?>" class="btn btn-default">Cancel</a>
-	</form>
-    </section>
-    <!-- /.content -->
-  </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <h2 style="margin-top:0px">Qc <?php echo $button ?></h2>
+            <form action="<?php echo $action; ?>" method="post">
+                <div class="form-group">
+                    <label for="int">Id Detail Spk <?php echo form_error('id_detail_spk') ?></label>
+                    <input type="text" class="form-control" name="id_detail_spk" id="id_detail_spk" placeholder="Id Detail Spk" value="<?php echo $id_detail_spk; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">No Spk <?php echo form_error('no_spk') ?></label>
+                    <input type="text" class="form-control" name="no_spk" id="no_spk" placeholder="No Spk" value="<?php echo $no_spk; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">No Item <?php echo form_error('no_item') ?></label>
+                    <input type="text" class="form-control" name="no_item" id="no_item" placeholder="No Item" value="<?php echo $no_item; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="date">Tgl Masuk <?php echo form_error('tgl_masuk') ?></label>
+                    <input type="text" class="form-control" name="tgl_masuk" id="tgl_masuk" placeholder="Tgl Masuk" value="<?php echo $this->session->userdata('tgl'); ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="int">Qty <?php echo form_error('qty') ?></label>
+                    <input type="text" class="form-control" name="qty" id="qty" placeholder="Qty" value="<?php echo $qty; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">Status <?php echo form_error('status') ?></label>
+                    <input type="text" class="form-control" name="status" id="status" placeholder="Status" value="<?php echo $status; ?>" />
+                </div>
+                <div class="form-group">
+                    <label for="varchar">Ket <?php echo form_error('ket') ?></label>
+                    <input type="text" class="form-control" name="ket" id="ket" placeholder="Ket" value="<?php echo $ket; ?>" />
+                </div>
+                <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                <button type="submit" class="btn btn-primary"><?php echo $button ?></button>
+                <a href="<?php echo site_url('qc') ?>" class="btn btn-default">Cancel</a>
+            </form>
+        </section>
+        <!-- /.content -->
+    </div>
 
     <?php $this->load->view('template/backend/footer') ?>
